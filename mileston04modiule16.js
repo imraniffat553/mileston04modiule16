bb62d57d2d2fc219f54a7d1ea70b2ad66a21fef8
imraniffat553 <imrannazer3@gmail.com>
2024-11-16 11:22:19 +0600

    to var sum work

--- mileston04modiule16.js
+++ mileston04modiule16.js
@@ -1,1 +1,14 @@
-console.log(99)
+var ChalNama=60;
+var NaMan=40;
+
+
+console.log(typeof NaMan);
+console.log(typeof ChalNama);
+
+// 2part 
+
+var ChalNama=60;
+var NaMan=40;
+
+
+console.log(ChalNama + NaMan);
